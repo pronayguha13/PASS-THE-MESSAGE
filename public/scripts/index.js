c8f5b691e4e20113b5138c7756871ac9c2db5b7a
pronayguha <pronay.guha@aot.edu.in>
2020-11-01 14:32:29 +0530

    getting input from user and displaying on the span defined for displaying messages

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,23 @@
-window.onload(alert("This is the boiler-plate"));
+//get the submit button
+const submitBtn = document.getElementById("submitButton");
+console.log("submitBtn", submitBtn);
+const inputBox = document.getElementById("messageBox");
+//assign the  handler function on enter key press
+inputBox.addEventListener("keypress", (e) => {
+  //detects whether the key pressed is Enter key or not
+  if (e.key === "Enter") {
+    onSubmitHandler();
+  }
+});
+//define function for onclick handler of the submit button and onEnterKeyPress in the input field
+const onSubmitHandler = () => {
+  //get the span element where messages will be displayed
+  const previousMessageContainer = document.getElementById(
+    "previousMessageContainer"
+  );
+  previousMessageContainer.innerHTML = inputBox.value
+    ? inputBox.value
+    : alert("Please Enter a message to pass");
+};
+//assign the function to the  submit button onclick
+submitBtn.addEventListener("click", onSubmitHandler); //the eventlistener takes reference of the handler function or an anonyous function
